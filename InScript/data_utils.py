aaf7fe2fa1075475762104e8e36cb2b8984264ae
Antonie Lin <iammrhelo@gmail.com>
2018-02-26 23:25:15 -0500

    process Inscript dataset

--- InScript/data_utils.py
+++ InScript/data_utils.py
@@ -1,56 +1,59 @@
 from glob import glob
 import os
 import random
+import shutil
 import xml.etree.ElementTree
 
 def parse_xml(xml_dir, output_file):
 
-    fout = open(output_file,'w')
+    with open(output_file,'w') as fout:
+        for xml_file in glob(os.path.join(xml_dir,'*.xml')):
+            print("Parsing {}...".format(xml_file))
+            root = xml.etree.ElementTree.parse(xml_file).getroot()
+            fout.write(root[0][0].text + '\n')
 
-    for xml_file in glob(os.path.join(xml_dir,"*.xml")):
-        print("Parsing {}...".format(xml_file))
-        scripts = xml.etree.ElementTree.parse(xml_file).getroot()
+def split_datasets(xml_dirs, output_dir):
+    # Train 
+    train_dir = os.path.join(output_dir,'train')
+    valid_dir = os.path.join(output_dir,'valid')
+    test_dir = os.path.join(output_dir,'test')
 
-        for script in scripts:
-            for item in script:
-                original = item.attrib["original"]
-                fout.write(original + '\n')
+    for dirname in [ train_dir, valid_dir, test_dir ]:
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
 
-    fout.close()
+    test_idx = random.randint(0,9)
+    valid_idx1 = random.randint(0,9)
+    valid_idx2 = random.randint(0,9)
+    for idx, xml_dir in enumerate(xml_dirs):
+        xml_files = glob(os.path.join(xml_dir,"*.xml"))
+        
+        random.shuffle(xml_files)
 
+        test_end = 18 + ( idx == test_idx )
+        valid_end = test_end + 9 + ( idx == valid_idx1 ) + ( idx == valid_idx2 )
 
-def random_split(text_file, train_file, valid_file, test_file):
-    
-    with open(text_file,'r') as fin:
-        lines = fin.read().split('\n')
+        test_files = xml_files[:test_end]
+        valid_files = xml_files[test_end:valid_end]
+        train_files = xml_files[valid_end:]
 
-    random.shuffle(lines)
-
-    length = len(lines)
-
-    train_end = int(length * 0.8)
-
-    valid_begin = train_end
-    valid_end = int(length * 0.9)
-
-    test_begin = valid_end
-
-    with open(train_file,'w') as fout:
-        fout.write('\n'.join(lines[:train_end]))
-    
-    with open(valid_file,'w') as fout:
-        fout.write('\n'.join(lines[valid_begin:valid_end]))
-    
-    with open(test_file,'w') as fout:
-        fout.write('\n'.join(lines[test_begin:]))
+        for copy_files, copy_dir in [(train_files,train_dir), (valid_files, valid_dir), (test_files, test_dir)]:
+            for copy_file in copy_files:
+                shutil.copy(copy_file, copy_dir)
 
 
 if __name__ == "__main__":
 
-    name = "second_esd"
+    name = "InScript"
+    output_dir = "data/InScript"
+    xml_dirs = list(filter(os.path.isdir,glob("InScript/corpus/*")))
 
-    xml_dir = os.path.join("DeScript_LREC2016/esds/",name)
+    split_datasets(xml_dirs, output_dir)
 
-    parse_xml(xml_dir, name + '.txt')
+    train_xml_dirs = os.path.join(output_dir,'train')
+    valid_xml_dirs = os.path.join(output_dir,'valid')
+    test_xml_dirs = os.path.join(output_dir,'test')
 
-    random_split(name + '.txt', name + "_train.txt", name + "_valid.txt", name + "_test.txt")
+    parse_xml(train_xml_dirs, os.path.join(output_dir,'inscript_train.txt'))
+    parse_xml(valid_xml_dirs, os.path.join(output_dir,'inscript_valid.txt'))
+    parse_xml(test_xml_dirs, os.path.join(output_dir,'inscript_test.txt'))
