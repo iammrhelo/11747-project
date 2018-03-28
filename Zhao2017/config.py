efbf197ae8f2aa17099dcba299605633b3391261
yulundu <yulundu@gmail.com>
2018-03-28 08:27:07 -0400

    fix validation bug

--- Zhao2017/config.py
+++ Zhao2017/config.py
@@ -5,55 +5,29 @@ def init_config():
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=321, type=int, help='random seed')
     parser.add_argument('--cuda', action='store_true', default=True, help='use gpu')
-    parser.add_argument('--mode', choices=['train', 'raml_train', 'test', 'sample', 'prob', 'interactive'],
-                        default='train', help='run mode')
     parser.add_argument('--vocab', default='./data/vocab.bin', type=str, help='path of the serialized vocabulary')
-    parser.add_argument('--batch_size', default=2, type=int, help='batch size')
+    parser.add_argument('--batch_size', default=64, type=int, help='batch size')
     parser.add_argument('--beam_size', default=5, type=int, help='beam size for beam search')
     parser.add_argument('--sample_size', default=10, type=int, help='sample size')
-    parser.add_argument('--embed_size', default=256, type=int, help='size of word embeddings')
-    parser.add_argument('--hidden_size', default=256, type=int, help='size of LSTM hidden states')
-    parser.add_argument('--dropout', default=0., type=float, help='dropout rate')
+    parser.add_argument('--embed_size', default=512, type=int, help='size of word embeddings')
+    parser.add_argument('--hidden_size', default=512, type=int, help='size of LSTM hidden states')
+    parser.add_argument('--dropout', default=0.5, type=float, help='dropout rate')
 
-    parser.add_argument('--train_src', type=str, help='path to the training source file')
-    parser.add_argument('--train_tgt', type=str, help='path to the training target file')
-    parser.add_argument('--dev_src', type=str, help='path to the dev source file')
-    parser.add_argument('--dev_tgt', type=str, help='path to the dev target file')
-    parser.add_argument('--test_src', type=str, help='path to the test source file')
-    parser.add_argument('--test_tgt', type=str, help='path to the test target file')
 
     parser.add_argument('--decode_max_time_step', default=200, type=int, help='maximum number of time steps used '
                                                                               'in decoding and sampling')
 
-    parser.add_argument('--valid_niter', default=500, type=int, help='every n iterations to perform validation')
+    parser.add_argument('--valid_nepoch', default=1, type=int, help='every n iterations to perform validation')
     parser.add_argument('--valid_metric', default='bleu', choices=['bleu', 'ppl', 'word_acc', 'sent_acc'], help='metric used for validation')
-    parser.add_argument('--log_every', default=50, type=int, help='every n iterations to log training statistics')
     parser.add_argument('--load_model', default=None, type=str, help='load a pre-trained model')
-    parser.add_argument('--save_to', default='model', type=str, help='save trained model to')
-    parser.add_argument('--save_model_after', default=2, help='save the model only after n validation iterations')
-    parser.add_argument('--save_to_file', default=None, type=str, help='if provided, save decoding results to file')
-    parser.add_argument('--save_nbest', default=False, action='store_true', help='save nbest decoding results')
+    parser.add_argument('--save_to', default='./model/', type=str, help='save trained model to')
+    parser.add_argument('--save_model_after', default=5, help='save the model only after n validation iterations')
     parser.add_argument('--patience', default=5, type=int, help='training patience')
     parser.add_argument('--uniform_init', default=None, type=float, help='if specified, use uniform initialization for all parameters')
     parser.add_argument('--clip_grad', default=5., type=float, help='clip gradients')
     parser.add_argument('--max_niter', default=-1, type=int, help='maximum number of training iterations')
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
     parser.add_argument('--lr_decay', default=0.5, type=float, help='decay learning rate if the validation performance drops')
-
-    # raml training
-    parser.add_argument('--debug', default=False, action='store_true')
-    parser.add_argument('--temp', default=0.85, type=float, help='temperature in reward distribution')
-    parser.add_argument('--raml_sample_mode', default='pre_sample',
-                        choices=['pre_sample', 'hamming_distance', 'hamming_distance_impt_sample'],
-                        help='sample mode when using RAML')
-    parser.add_argument('--raml_sample_file', type=str, help='path to the sampled targets')
-    parser.add_argument('--raml_bias_groundtruth', action='store_true', default=False, help='make sure ground truth y* is in samples')
-
-    parser.add_argument('--smooth_bleu', action='store_true', default=False,
-                        help='smooth sentence level BLEU score.')
-
-    #TODO: greedy sampling is still buggy!
-    parser.add_argument('--sample_method', default='random', choices=['random', 'greedy'])
 
     args = parser.parse_args()
 
