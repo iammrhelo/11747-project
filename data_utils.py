bf33d518a60459a0beda8c20b6e348e9ab630300
Antonie Lin <iammrhelo@gmail.com>
2018-05-02 00:21:06 -0400

    merge data loaders

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 import copy
+from glob import glob
+import os
 import pickle as pkl
-import string
+import xml.etree.ElementTree
 
 import numpy as np
 import torch
@@ -10,6 +12,117 @@ from vocab import Vocab, VocabEntry
 
 np.set_printoptions(precision=3)
 use_cuda = torch.cuda.is_available()
+
+
+def to_tensor(doc):
+    X, R, E, L = doc[0]
+    
+    tX = []
+    tR = []
+    tE = []
+    tL = []
+
+    for sent_idx in range(len(X)):
+    
+        tx = torch.from_numpy(np.array(X[sent_idx]))
+        tr = torch.from_numpy(np.array(R[sent_idx]))
+        te = torch.from_numpy(np.array(E[sent_idx]))
+        tl = torch.from_numpy(np.array(L[sent_idx]))      
+
+        if use_cuda:
+            tx = tx.cuda()
+            tr = tr.cuda()
+            te = te.cuda()
+            tl = tl.cuda()
+
+        tX.append(tx)
+        tR.append(tr)
+        tE.append(te)
+        tL.append(tl)      
+
+    return [(tX, tR, tE, tL)]
+
+
+
+class InScriptDataLoader(object):
+    def __init__(self, xml_dir, dict_pickle):
+        self.name = os.path.basename(xml_dir)
+        self.use_cuda = use_cuda
+
+        # Use Predefined Dictionary 
+        with open(dict_pickle,'rb') as fin:
+            self.dictionary = pkl.load(fin)
+        
+        self.documents = []
+
+        for xml_file in sorted(glob(os.path.join(xml_dir,"*.xml"))):
+            doc_name = os.path.basename(xml_file)
+            doc = self.parse_document(xml_file, self.dictionary)
+            tensor_doc = to_tensor(doc)
+            self.documents.append((doc_name,tensor_doc))
+
+    def parse_document(self, xml_file, dictionary, debug=False):
+        root = xml.etree.ElementTree.parse(xml_file).getroot()
+
+        content = root[0][0].text
+        sentences = content.split('\n')
+
+        R = []
+        E = [] 
+        L = []
+
+        participants = root[1][0]
+
+        entity_table = {}
+
+        for sent in sentences:
+            R.append([0] * len(sent.split()))
+            E.append([0] * len(sent.split())) 
+            L.append([1] * len(sent.split())) 
+
+        for label in participants:
+            sentence_id, word_id = map(int, label.attrib['from'].split('-'))
+            
+            entity = label.attrib["name"]
+
+            text = label.attrib["text"]
+            tokens = text.split()
+
+            start = word_id-1
+            end = start+1
+            if 'to' in label.attrib:
+                _, end = map(int,label.attrib['to'].split('-'))
+
+            if any(x == 0 for x in E[sentence_id-1][start:end]):
+            
+                if entity not in entity_table:
+                    entity_table[entity] = len(entity_table)+1
+                entity_id = entity_table[entity]
+
+                # R : is entity?
+                # E : entity index
+
+                for idx in range(start,end,1):
+                    E[sentence_id-1][idx] = entity_id
+                    R[sentence_id-1][word_id-1] = 1
+
+                # L : entity remaining length
+                for l in range(len(tokens),0,-1):
+                    idx = word_id-1 + len(tokens)-l
+                    L[sentence_id-1][idx] = l
+
+        X = []
+        for sent in sentences:
+            x = []
+            for word in sent.split():
+                xidx = self.dictionary.get(word,0)
+                x.append(xidx)
+            X.append(x)
+        
+        doc = []
+        doc.append((X,R,E,L))
+        return doc
+
 
 class LetsGoCorpus(object):
 
@@ -61,7 +174,6 @@ def read_corpus_vocab(corp, source):
 
     return data
 
-
 def data_iter(data, batch_size, shuffle=True):
     """
     randomly permute data, then sort by source length, and partition into batches
@@ -97,79 +209,29 @@ def batch_slice(data, batch_size, sort=True):
 
         yield src_sents, tgt_sents
 
-# Data feed
-class DataLoader(object):
-    batch_size = 0
-    ptr = 0
-    num_batch = None
-    batch_indexes = None
-    indexes = None
-    data_size = None
-    name = None
-    equal_len_batch = None
-    # if equal batch, the indexes sorted by data length
-    # else indexes = range(data_size)
-
-    def _shuffle_indexes(self):
-        np.random.shuffle(self.indexes)
-
-    def _shuffle_batch_indexes(self):
-        np.random.shuffle(self.batch_indexes)
-
-    def _prepare_batch(self, selected_indexes):
-        raise NotImplementedError("Have to override prepare batch")
-
-    def epoch_init(self, batch_size, shuffle=True):
-        if self.name.upper() == "TEST":
-            new_batch_size = None
-            for i in range(3):
-                factors = utils.factors(self.data_size-i)
-                temp = min(factors, key=lambda x:abs(x-batch_size))
-                if np.abs(temp-batch_size) < batch_size * 0.5:
-                    new_batch_size = temp
-                    break
-            if new_batch_size is not None:
-                batch_size = new_batch_size
-                print("Adjust the batch size to %d" % batch_size)
-
-        self.ptr = 0
-        self.batch_size = batch_size
-        self.num_batch = self.data_size // batch_size
-        print("Number of left over sample %d" % (self.data_size-batch_size*self.num_batch))
-
-        # if shuffle and we don't want to group lines, shuffle index
-        if shuffle and not self.equal_len_batch:
-            self._shuffle_indexes()
-
-        self.batch_indexes = []
-        for i in range(self.num_batch):
-            self.batch_indexes.append(self.indexes[i * self.batch_size:(i + 1) * self.batch_size])
-
-        # if shuffle and we want to group lines, shuffle batch indexes
-        if shuffle and self.equal_len_batch:
-            self._shuffle_batch_indexes()
-
-        print("%s begins with %d batches" % (self.name, self.num_batch))
-
-    def next_batch(self):
-        if self.ptr < self.num_batch:
-            selected_ids = self.batch_indexes[self.ptr]
-            self.ptr += 1
-            return self._prepare_batch(selected_indexes=selected_ids)
-        else:
-            return None
-
+"""
+Data Loader for Lets Go Corpus
+For EntityNLM, 
+    I:  Whole Dialogue as document => 4k
+    II: Previous context and target sentence => 55k
+"""
 class LetsGoDataLoader():
     def __init__(self, data, vocab):
 
         self.data = data
+        self.vocab = vocab
+        
+        # For encoder decoder
         self.src = []
         self.tgt = []
         self.max_len = 30
-        
-        self.vocab = vocab
         self.process()
 
+        # For Entity NLM
+        self.documents = []
+        self.entities = []
+        self.build_documents()
+       
     def process(self):
         for dial in self.data:
             for i, turn in enumerate(dial):
@@ -186,27 +248,20 @@ class LetsGoDataLoader():
                 self.tgt.append(['<s>'] + turn[0].strip().split(' ') + ['</s>'])
 
     def build_documents(self):
-        self.documents = []
-        self.entities = []
-        for dial_idx, dial in enumerate(self.src,1):
+        
+        for dial_idx, dial in enumerate(self.data,1):
+            # One Document
             entity_dict = {}
             X_s = []
             R_s = []
             E_s = []
             L_s = []
             for i, turn in enumerate(dial):
-           
-                sys = turn[0]
-                usr = turn[1]
-
-                assert len(sys) and len(usr)
-
-                if sys[-1] not in string.punctuation:
-                    sys.append('.')
-                if usr[-1] not in string.punctuation:
-                    usr.append('.')
-
-                assert len(sys) > 1 and len(usr) > 1
+                sys = turn[0].strip().split(' ')
+                usr = turn[1].strip().split(' ')
+                
+                sys = ['<s>'] + sys + ['</s>']
+                usr = ['<s>'] + usr + ['</s>']
 
                 sysR = self.get_R(sys)
                 usrR = self.get_R(usr)
@@ -217,8 +272,8 @@ class LetsGoDataLoader():
                 sysE = self.get_E(sys, entity_dict)
                 usrE = self.get_E(usr, entity_dict)
             
-                sys = [self.vocab[w] for w in sys]
-                usr = [self.vocab[w] for w in usr]
+                sys = [ self.vocab[w] for w in sys ]
+                usr = [ self.vocab[w] for w in usr ]
                 
                 X_s.append(sys)
                 X_s.append(usr)
@@ -233,10 +288,11 @@ class LetsGoDataLoader():
             assert len(X_s) % 2 == 0 
             doc = [ (X_s, R_s, E_s, L_s) ]
 
-            tensor_doc = self.to_tensor(doc)
-
+            # Convert to Tensor
+            tensor_doc = to_tensor(doc)
+            
+            # Appent to self
             self.documents.append((str(dial_idx), tensor_doc))
-
             self.entities.append(entity_dict)
 
     def get_src(self):
@@ -246,14 +302,19 @@ class LetsGoDataLoader():
         return self.tgt
 
     def get_R(self, sent):
-        ret = [ int('<' in word) for word in sent]
+        ret = []
+        for word in sent:
+            if word.startswith('<') and word not in ['<s>','</s>']:
+                ret.append(1)
+            else:
+                ret.append(0)
         return ret
     
     def get_E(self, sent, entity_dict):
         ret = []
         for word in sent:
             # Check if is entity
-            if '<' in word:
+            if word.startswith('<') and word not in ['<s>','</s>']:
                 if word not in entity_dict: 
                     entity_dict[ word ] = len(entity_dict)+1
                 entity_idx = entity_dict.get(word)
@@ -297,184 +358,22 @@ class LetsGoDataLoader():
 
         return [(tX, tR, tE, tL)]
 
-class LetsGoEntityDataLoader():
-    def __init__(self, data, vocab, use_cuda=False):
-
-        self.data = data
-        self.dialogs = []
-        self.R = []
-        self.E = []
-        self.L = []
-        self.entity = [] # invalid entity
-        self.vocab = vocab
-        
-        self.use_cuda = use_cuda
-
-        self.documents, self.entities = self.process()
-        
-    def process(self):
-        documents = []
-        entities = []
-        for dial_idx, dial in enumerate(self.data,1):
-            # One Document
-            entity_dict = {}
-            X_s = []
-            R_s = []
-            E_s = []
-            L_s = []
-            for i, turn in enumerate(dial):
-                sys = turn[0].strip().split(' ')
-                usr = turn[1].strip().split(' ')
-                
-                assert len(sys) and len(usr)
-
-                if sys[-1] not in string.punctuation:
-                    sys.append('.')
-                if usr[-1] not in string.punctuation:
-                    usr.append('.')
-
-                assert len(sys) > 1 and len(usr) > 1
-
-                sysR = self.get_R(sys)
-                usrR = self.get_R(usr)
-                
-                sysL = self.get_L(sys)
-                usrL = self.get_L(usr)
-                
-                sysE = self.get_E(sys, entity_dict)
-                usrE = self.get_E(usr, entity_dict)
-            
-                sys = [self.vocab[w] for w in sys]
-                usr = [self.vocab[w] for w in usr]
-                
-                X_s.append(sys)
-                X_s.append(usr)
-                R_s.append(sysR)
-                R_s.append(usrR)
-                E_s.append(sysE)
-                E_s.append(usrE)
-                L_s.append(sysL)
-                L_s.append(usrL)
-            
-
-            assert len(X_s) % 2 == 0 
-            doc = [ (X_s, R_s, E_s, L_s) ]
-
-            tensor_doc = self.to_tensor(doc)
-
-            documents.append((str(dial_idx), tensor_doc))
-
-            entities.append(entity_dict)
-        
-        return documents, entities
-
-
-    def get_R(self, sent):
-        ret = [ int('<' in word) for word in sent]
-        return ret
-    
-    def get_E(self, sent, entity_dict):
-        ret = []
-        for word in sent:
-            # Check if is entity
-            if '<' in word:
-                if word not in entity_dict: 
-                    entity_dict[ word ] = len(entity_dict)+1
-                entity_idx = entity_dict.get(word)
-            else:
-                entity_idx = 0
-            ret.append(entity_idx)
-        
-        return ret
-    
-    def get_L(self, sent):
-        ret = [1 for _ in sent]
-        return ret
-
-    def to_tensor(self, doc):
-        X, R, E, L = doc[0]
-        
-        tX = []
-        tR = []
-        tE = []
-        tL = []
-
-        for sent_idx in range(len(X)):
-        
-            tx = torch.from_numpy(np.array(X[sent_idx]))
-            tr = torch.from_numpy(np.array(R[sent_idx]))
-            te = torch.from_numpy(np.array(E[sent_idx]))
-            tl = torch.from_numpy(np.array(L[sent_idx]))      
-
-            if self.use_cuda:
-                tx = tx.cuda()
-                tr = tr.cuda()
-                te = te.cuda()
-                tl = tl.cuda()
-
-            tX.append(tx)
-            tR.append(tr)
-            tE.append(te)
-            tL.append(tl)      
-        
-        assert len(tX) % 2 == 0 
-
-        return [(tX, tR, tE, tL)]
-
-    def display_stats(self):
-        # Document 
-        nwords = [] 
-        nentities = []  
-        nentities_distinct = []     
-
-        # Sentence 
-        nwords_sentence = []
-        nentities_sentence = []
-        for (doc_name, doc) in self.documents:
-
-            X, R, E, L = doc[0]
-
-            word_count = 0
-            entity_count = 0
-            for sent_x, sent_r in zip(X,R):
-                word_count += sent_x.size()[0]
-                entity_count += sent_r.sum()
-                nwords_sentence.append(sent_x.size()[0])
-                nentities_sentence.append(sent_r.sum())
-
-            nwords.append(word_count)
-            nentities.append(entity_count)
-        
-        nentities_distinct = [ len(entity_dict) for entity_dict in self.entities ]
-
-        def print_mean_std(name, stats):
-            mean = np.mean(stats)
-            std = np.std(stats)
-            maximum = np.max(stats)
-            mininum = np.min(stats)
-
-            msg = ""
-            msg += "{0:.3f}".format(mean)
-            msg += "(+/-{0:.3f})".format(std)
-            msg += ", max {}".format(maximum)
-            msg += ", min {}".format(mininum)
-            print(name,msg)
-
-        print_mean_std("Average number of words per document", nwords)
-        print_mean_std("Average number of entities per document", nentities)
-        print_mean_std("Average number of distinct entities per document", nentities_distinct)
-        print_mean_std("Average number of words per sentences", nwords_sentence)
-        print_mean_std("Average number of entites per sentence", nentities_sentence)
-
 def load_corpus(args):
     dataset_name = args.dataset
     print("Loading dataset",dataset_name)
-    if dataset_name == "letsgo":
+    if dataset_name == "inscript":
+        data_dir = './data/modi'
+        dict_pickle = os.path.join(data_dir,'train','dict.pickle')
+        train_corpus = InScriptDataLoader(os.path.join(data_dir,'train'), dict_pickle)
+        valid_corpus = InScriptDataLoader(os.path.join(data_dir,'valid'), dict_pickle)
+        test_corpus = InScriptDataLoader(os.path.join(data_dir,'test'), dict_pickle)
+        dictionary = train_corpus.dictionary
+    elif dataset_name == "letsgo":
         vocab = torch.load('./data/vocab.bin')
         corpus = LetsGoCorpus('./data/union_data-1ab.p')
-        train_corpus = LetsGoEntityDataLoader(corpus.train, vocab.src, use_cuda=use_cuda)
-        valid_corpus = LetsGoEntityDataLoader(corpus.valid, vocab.src, use_cuda=use_cuda)
-        test_corpus = LetsGoEntityDataLoader(corpus.test, vocab.src, use_cuda=use_cuda)
+        train_corpus = LetsGoDataLoader(corpus.train, vocab.src)
+        valid_corpus = LetsGoDataLoader(corpus.valid, vocab.src)
+        test_corpus = LetsGoDataLoader(corpus.test, vocab.src)
         dictionary = train_corpus.vocab.word2id
     else:
         raise ValueError("Invalid dataset:",dataset_name)
@@ -485,19 +384,6 @@ if __name__ == "__main__":
     vocab = torch.load('./data/vocab.bin')
     corpus = LetsGoCorpus('./data/union_data-1ab.p')
     test_loader = LetsGoDataLoader(corpus.test, vocab.src)
-    test_loader.build_documents()
-    """
-    train_loader = LetsGoEntityDataLoader(corpus.train, vocab.src)
-    print("Train")
-    train_loader.display_stats()
-    valid_loader = LetsGoEntityDataLoader(corpus.valid, vocab.src)
-    print("Valid")
-    valid_loader.display_stats()
-
-    test_loader = LetsGoEntityDataLoader(corpus.test, vocab.src)
-    print("Test")
-    test_loader.display_stats()
-    """
 
 
 
